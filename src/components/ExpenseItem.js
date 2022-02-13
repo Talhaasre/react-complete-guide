26de9a8d2d04bee75a87b8784f1ddd28c188bb23
talhatoggle <talha@togglehead.in>
2022-02-14 02:49:41 +0530

    module 3

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./expenseitem.scss";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  return (
+    <>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          {/* <h2>{1+1}</h2>
+          <h2>{Math.random()}</h2> */}
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </>
+  );
+}
+
+export default ExpenseItem;
